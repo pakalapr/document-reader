e5fd2d5b0b62b9f861d46a10ee9a7b1a19f0b318
DESKTOP-MTU2213\pakal <Pakala.Prakash10@T-Mobile.com>
2025-01-31 16:21:09 -0600

    devop changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const port = process.env.PORT || 3000
 
 //middleware
 app.use(cors());
@@ -111,6 +112,6 @@ app.post("/document", async (req, res) => {
 // });
 
 //app.listen(5000, () => {
-app.listen(3000, () => {
-  console.log("server has started on port 3000");
+app.listen(port, () => {
+  console.log("server has started on port:3000");  
 });
